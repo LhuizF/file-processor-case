6446a87eb3cc9d44672526d059f72d9bff1d13cf
Luiz Henrique Ferreira <luizhff@gmail.com>
2025-08-31 19:44:51 -0300

    feat: CORS

--- backend/src/FileProcessor.API/Program.cs
+++ backend/src/FileProcessor.API/Program.cs
@@ -15,6 +15,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowAll", policy =>
+  {
+    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+  });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
@@ -57,6 +65,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/health", () => Results.Ok());
 
